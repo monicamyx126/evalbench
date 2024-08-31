dfffd3ef5e41f795c8b522997b0c0476042eac1f
vidit chopra <viditchopra@google.com>
2024-08-31 04:09:08 +0000

    Fixed formatting issues.

--- evalbench/scorers/llmrater.py
+++ evalbench/scorers/llmrater.py
@@ -1,12 +1,11 @@
 """
-Currently the LLM Rater compares the golden execution results with the 
+Currently the LLM Rater compares the golden execution results with the
 generated sql execution results. It returns a score of 100 for concrete
-positive cases, where either there is a Mismatch of Columns names or Extra Relevant 
+positive cases, where either there is a Mismatch of Columns names or Extra Relevant
 Columns in Generated SQL exists.
 """
 
 from typing import Tuple
-import json
 import logging
 
 from scorers import comparator
@@ -69,11 +68,14 @@ class LLMRater(comparator.Comparator):
         {generated_execution_result}
 
 
-        Thinking step by step, compare the two outputs and look for differences in data presentation. Here are steps to follow:
+        Thinking step by step, compare the two outputs and look for differences in data presentation.
+        Here are steps to follow:
 
-        1) Map all columns in OUTPUT #1 to the same columns in OUTPUT #2. Their names might be different so long as the content represents the same data.
+        1) Map all columns in OUTPUT #1 to the same columns in OUTPUT #2. Their names might be different
+           so long as the content represents the same data.
         2) If all columns in OUTPUT #1 do not map to OUTPUT #2 then information is missing.
-        3) Compare the data in the mapped columns between OUTPUT #1 and OUTPUT #2. The data should be an exact match, there should be no extra or missing data in OUTPUT #2
+        3) Compare the data in the mapped columns between OUTPUT #1 and OUTPUT #2. The data should be an
+           exact match, there should be no extra or missing data in OUTPUT #2
 
         Rules:
         1) Always assume that OUTPUT #1 is correct.
@@ -88,7 +90,8 @@ class LLMRater(comparator.Comparator):
         - INFORMATION_MATCHES -- OUTPUT #1 and OUTPUT #2 provide the same information.
         - MISSING_INFORMATION -- Something important is missing from OUTPUT #2.
         - EXTRA_INFORMATION -- Some non-harmful extra relevant columns were added to OUTPUT #2.
-        - INCORRECT_INFORMATION -- Some incorrect information was added to OUTPUT #2, likely due to an incorrect filter or incorrect aggregation.
+        - INCORRECT_INFORMATION -- Some incorrect information was added to OUTPUT #2, likely due to an incorrect
+          filter or incorrect aggregation.
 
         """
         logging.debug("\n --------- prompt:   --------- \n %s ", prompt)
@@ -97,5 +100,9 @@ class LLMRater(comparator.Comparator):
         ).text
 
         logging.debug("\n --------- llm_rater_output:   --------- \n %s ", response)
-        score = 100 if ('INFORMATION_MATCHES' in response or 'EXTRA_INFORMATION' in response) else 0
+        score = (
+            100
+            if ("INFORMATION_MATCHES" in response or "EXTRA_INFORMATION" in response)
+            else 0
+        )
         return score, response
