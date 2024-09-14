ec9e001ce983ace88d381c8225f789da9a7bb6ef
Hardik Gupta <hardikgupta@google.com>
2024-09-14 17:31:39 +0530

    Compare checksum when setting up database

--- evalbench/setup_teardown/setup_teardown.py
+++ evalbench/setup_teardown/setup_teardown.py
@@ -2,6 +2,7 @@ import os
 import sys
 import importlib
 import logging
+import csv
 import pandas as pd
 from google.protobuf import text_format
 from .schema_details.schema_detail_pb2 import SchemaDetails
@@ -34,7 +35,7 @@ def save_checksums_to_csv(checksums, output_file):
 def calculate_checksum(db_config):
     db_handler = databaseHandler.get_db_handler(db_config)
     setup_file = os.path.join(
-        os.path.dirname(__file__), 
+        os.path.dirname(__file__),
         f"schema_details/bat/{db_config['database_name']}/setup.yaml"
     )
     setup = util_config.load_yaml_config(setup_file)
@@ -49,7 +50,7 @@ def calculate_checksum(db_config):
         return
 
     output_file = os.path.join(
-        os.path.dirname(__file__), 
+        os.path.dirname(__file__),
         f"checksum/{db_config['database_name']}_{db_engine}.csv"
     )
     save_checksums_to_csv(result, output_file)
@@ -84,6 +85,8 @@ def setupDatabase(db_config: dict):
     for section in ["pre_setup", "post_schema_creation", "post_setup", "post_data_insertion_checks"]:
         commands = setup['setup_commands'][section][db_engine]
         setup_commands[section].extend(commands)
+    commands = setup_commands["post_data_insertion_checks"]
+    setup_commands["post_data_insertion_checks"] = [" UNION ALL ".join(commands) + ";"]
 
     schema_file = os.path.join(os.path.dirname(__file__), f"schema_details/bat/{database_name}/{db_engine}.textproto")
     schema = parse_textproto_file(schema_file)
@@ -100,5 +103,16 @@ def setupDatabase(db_config: dict):
         if error is not None:
             logging.error(f"Error in section {section}: {error}")
             return
+        if section == "post_data_insertion_checks":
+            expected_checksum_csv = os.path.join(os.path.dirname(__file__),
+                                                 f"checksum/{db_config['database_name']}_{db_engine}.csv")
+            with open(expected_checksum_csv, 'r') as file:
+                csv_reader = csv.DictReader(file)
+                csv_data_set = set((row['table_name'], row['checksum'] or None) for row in csv_reader)
+                result_set = set((entry['table_name'], entry['checksum'] or None) for entry in result)
+
+                if not csv_data_set == result_set:
+                    logging.error("Checksums do not match.")
+                    return
 
     logging.info("Setup completed successfully.")
