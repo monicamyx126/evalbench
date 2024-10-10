e19370e68a7fa29671673a76b930918c565ea614
Hardik Gupta <hardikgupta@google.com>
2024-10-10 16:11:26 +0530

    Remove gen_id

--- evalbench/dataset/evalinput.py
+++ evalbench/dataset/evalinput.py
@@ -1,5 +1,5 @@
 from google.protobuf.json_format import MessageToDict
-import eval_request_pb2
+# import eval_request_pb2
 
 
 class EvalInputRequest:
@@ -42,25 +42,25 @@ class EvalInputRequest:
         self.generated_sql = generated_sql
         self.job_id = job_id
 
-    @classmethod
-    def init_from_proto(self, proto: eval_request_pb2.EvalInputRequest):
-        """Initializes an EvalInputRequest from eval_request_pb2 proto."""
+    # @classmethod
+    # def init_from_proto(self, proto: eval_request_pb2.EvalInputRequest):
+    #     """Initializes an EvalInputRequest from eval_request_pb2 proto."""
 
-        request = MessageToDict(proto)
-        return self(
-            id=request.get("id"),
-            query_type=request.get("queryType"),
-            database=request.get("database"),
-            nl_prompt=request.get("nlPrompt"),
-            dialects=request.get("dialects"),
-            golden_sql=request.get("goldenSql"),
-            eval_query=request.get("evalQuery"),
-            setup_sql=request.get("setupSql"),
-            cleanup_sql=request.get("cleanupSql"),
-            tags=request.get("tags"),
-            other=request.get("other"),
-            sql_generator_error=request.get("sqlGeneratorError"),
-            sql_generator_time=request.get("sqlGeneratorTime"),
-            generated_sql=request.get("generatedSql"),
-            job_id=request.get("jobId"),
-        )
+    #     request = MessageToDict(proto)
+    #     return self(
+    #         id=request.get("id"),
+    #         query_type=request.get("queryType"),
+    #         database=request.get("database"),
+    #         nl_prompt=request.get("nlPrompt"),
+    #         dialects=request.get("dialects"),
+    #         golden_sql=request.get("goldenSql"),
+    #         eval_query=request.get("evalQuery"),
+    #         setup_sql=request.get("setupSql"),
+    #         cleanup_sql=request.get("cleanupSql"),
+    #         tags=request.get("tags"),
+    #         other=request.get("other"),
+    #         sql_generator_error=request.get("sqlGeneratorError"),
+    #         sql_generator_time=request.get("sqlGeneratorTime"),
+    #         generated_sql=request.get("generatedSql"),
+    #         job_id=request.get("jobId"),
+    #     )
