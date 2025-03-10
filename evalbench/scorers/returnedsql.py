cb80e188a855b43a336a3ae19cc6b97d384610ca
Divyansh Gupta <dvnshgupta@google.com>
2025-03-10 15:14:40 +0000

    fix and document returned SQL comparator

--- evalbench/scorers/returnedsql.py
+++ evalbench/scorers/returnedsql.py
@@ -1,11 +1,25 @@
+"""
+ReturnedSQL
+
+This comparison stragtegy checks if the generated sql query has any non-comment lines.
+It assigns a score of 100 if there are non-comment lines, otherwise a score of 0.
+
+It checks for both single line comments (start with --) and multiline comments (start with /*, end with */)
+
+Runtime Options: None
+"""
+
 from scorers import comparator
 from typing import Tuple
 
 
 class ReturnedSQL(comparator.Comparator):
-    """ReturnedSQL scorer checks if the generated SQL query contains anything except comments.
+    """
+    ReturnedSQL class implements the Comparator base class with SQL finding logic.
 
-    It assigns a score of 100 if there are non-comment lines, otherwise a score of 0.
+    Attributes:
+        1. name: Name of the comparator. Set to "returned_sql"
+        2. config: Scorer config defined in the run config yaml file
     """
 
     def __init__(self, config: dict):
@@ -25,13 +39,14 @@ class ReturnedSQL(comparator.Comparator):
         generated_eval_result: str,
         generated_error: str,
     ) -> Tuple[float, str]:
+        """compare function implements the SQL finding logic for ReturnedSQL comparator."""
 
         if generated_query == "":
             return 100, None
 
         query_lines = [line.strip() for line in generated_query.splitlines()]
         has_non_comment_line = any(
-            line and not line.startswith("--") for line in query_lines
+            line and (not line.startswith("--") and not line.startswith("/*")) for line in query_lines
         )
 
         score = 100 if has_non_comment_line else 0
