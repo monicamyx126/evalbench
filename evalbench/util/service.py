47affb2044f86034c61f0b276d49ef3a0f579b27
Mahyar Ebadi <mebadi@google.com>
2025-03-27 22:17:00 +0000

    Update configs to use a plural set of db_configs rather than one

--- evalbench/util/service.py
+++ evalbench/util/service.py
@@ -1,23 +1,13 @@
 from dataset import evalinput
-import databases
-import generators.models as models
-import generators.prompts as prompts
 
 
 def load_session_configs(session):
     return (
         session["config"],
-        session["db_config"],
+        session["db_configs"],
         session["model_config"],
         session["setup_config"],
     )
-
-
-def create_eval_instances(config, db_config, model_config):
-    core_db = databases.get_database(db_config)
-    model_generator = models.get_generator(model_config)
-    prompt_generator = prompts.get_generator(core_db, config)
-    return core_db, model_generator, prompt_generator
 
 
 async def get_dataset_from_request(request_iterator):
