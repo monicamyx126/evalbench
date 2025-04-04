de35946140c0f9f57f0199aa3f068dd19297d9b6
Jahnavi Malhotra <jahnaviii@google.com>
2025-04-04 19:54:31 +0000

    Minor query update

--- evalbench/util/sanitizer.py
+++ evalbench/util/sanitizer.py
@@ -1,7 +1,7 @@
 def sanitize_sql(sql: str):
     return (
-        sql.replace("```sql", "")  # required for gemini_1.0_pro, gemini_2.0_flash, gemini_2.0_pro
-        .replace("```", "")  # required for gemini_1.0_pro, gemini_2.0_flash, gemini_2.0_pro
+        sql.replace("```sql", "")  # required for gemini_1.0_pro, gemini_2.0_flash, gemini_2.5_pro
+        .replace("```", "")  # required for gemini_1.0_pro, gemini_2.0_flash, gemini_2.5_pro
         .replace('sql: "', "")
         .replace("\\n", " ")
         .replace("\\", "")
